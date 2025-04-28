62e49e467fc3637eaecfee7c1595ba4881e010a1
Graham-ki <grahamkimbugwe1738@gmail.com>
2025-04-28 16:19:19 +0300

    Created client to connect to database

--- src/app/admin/suppliers/page.tsx
+++ src/app/admin/suppliers/page.tsx
@@ -4,7 +4,10 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { createClient } from "@supabase/supabase-js";
-
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 interface Supplier {
   id: string;
   name: string;
